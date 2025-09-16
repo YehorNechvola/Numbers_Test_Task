71f84a7000a50dc69ddbc889f5254312181809cf
YehorNechvola <ynechvola@eva.ua>
2025-09-16 13:26:13 +0300

    Init commit

---
+++ Numbers_TestTask_Nechvola/Sources/NumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Numbers_TestTask_NechvolaApp.swift
+//  Numbers_TestTask_Nechvola
+//
+//  Created by Rush_user on 16.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct NumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
